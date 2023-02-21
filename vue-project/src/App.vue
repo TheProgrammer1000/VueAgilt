27ad00f20dba75128ff8258f5b2e32fda37cdb1c
dejv0ld <david_johnson_1992@hotmail.com>
2023-02-21 15:07:43 +0100

    Skapade mapparna components och images. Skapade hero med text.

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,17 +1,6 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-</script>
+<script setup></script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
 
