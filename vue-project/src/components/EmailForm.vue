5ff0b59dd3f33b056eac3117c50da77f6946a1ef
SimonFritzNguesseu <simonfritz3977@hotmail.com>
2023-02-22 13:21:05 +0100

    Created E-mail form

---
+++ vue-project/src/components/EmailForm.vue
@@ -0,0 +1,31 @@
+<script>
+import { required, email } from "vuelidate/lib/validators";
+
+export default {
+  data() {
+    return {
+      email: "",
+    };
+  },
+  validations: {
+    email: {
+      required,
+      email,
+    },
+  },
+  methods: {
+    submitForm() {
+      // Send form data to server using an HTTP request
+    },
+  },
+};
+</script>
+
+<template>
+  <form @submit.prevent="submitForm">
+    <label for="email">Email:</label>
+    <input id="email" v-model="email" type="email" required />
+    <span v-if="$v.email.$error">Please enter a valid email address.</span>
+    <button type="submit">Subscribe</button>
+  </form>
+</template>
