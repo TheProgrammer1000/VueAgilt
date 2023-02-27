21ef86ba1632f7e9de205b7043db6b255a96d166
SimonFritzNguesseu <simonfritz3977@hotmail.com>
2023-02-27 17:48:02 +0100

    Product page ready for styling and cart functions

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -30,7 +30,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/LandingPage.vue"),
-
+    },
+    {
+      path: "/productpage",
+      name: "productpage",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/ProductView.vue"),
     },
   ],
 });
