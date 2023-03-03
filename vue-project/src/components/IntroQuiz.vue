8e54511fcf68564b3c3fd2ee5a9e50365850ac56
Yavuziths <112006522+Yavuziths@users.noreply.github.com>
2023-03-03 18:46:37 +0100

    fixed so 50/50 gone after used

--- vue-project/src/components/IntroQuiz.vue
+++ vue-project/src/components/IntroQuiz.vue
@@ -119,7 +119,7 @@ export default {
           ],
           answer: "-270 celsius",
         },
-        // More questions can be added here
+        // kan lägga till fler frågor här
       ],
       currentQuestion: 0,
       selectedOption: "",
@@ -173,14 +173,24 @@ export default {
           this.selectedOption = "";
           this.selectedOptions = [];
           this.score = 0;
-          this.fiftyFiftyUsed = false;
           this.time = 15;
           this.timerVisible = true;
           this.timerRunning = false;
           this.tries += 1;
           this.errorDisplay = true;
           this.callRichardUsed = false;
-          this.startTimer(); // Start the timer on the first question
+          this.startTimer(); // startar tiden på första frågan buggfix //done
+
+          //   resettaar 50/50 button om current question är använd så försvinner den //done
+          const currentQuestion = this.questions[this.currentQuestion];
+          if (
+            currentQuestion.fiftyFiftyUsed &&
+            currentQuestion.answer !== this.selectedOptions[i]
+          ) {
+            currentQuestion.options = currentQuestion.originalOptions;
+            currentQuestion.fiftyFiftyUsed = false;
+            this.fiftyFiftyUsed = false;
+          }
         }
       } else {
         this.currentQuestion += 1;
@@ -189,18 +199,6 @@ export default {
           this.startTimer();
         }
       }
-    },
-    startTimer() {
-      this.timerRunning = true;
-      this.time = 15; // reset timer value
-      this.timerId = setInterval(() => {
-        if (this.time > 0) {
-          this.time--;
-        }
-        if (this.time === 0) {
-          this.checkAnswer();
-        }
-      }, 1000);
     },
 
     fiftyFifty() {
@@ -220,6 +218,20 @@ export default {
         //  Markerar att 50/50 knappen har blivit använd
         question.fiftyFiftyUsed = true;
       }
+    },
+
+    startTimer() {
+      this.timerRunning = true;
+      this.time = 15; // reset timer value
+      this.timerId = setInterval(() => {
+        if (this.time > 0) {
+          this.time--;
+        } else {
+          clearInterval(this.timerId);
+          this.timerRunning = false;
+          this.checkAnswer();
+        }
+      }, 1000);
     },
     callRichard() {
       if (!this.callRichardUsed) {
